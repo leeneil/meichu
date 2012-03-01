3abb1b417cdd91c748cdd674bb44ead7d15c6cdd
Po-Nan Li <lishantreasure@gmail.com>
2012-03-02 01:13:06 +0800

    group by issue

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
   def index
     @page_title = '首頁'
     @statuses = Status.all
-    @current_statuses = Game.all(:group=>"status_id")
+    #@current_statuses = Game.all(:group=>"status_id")
     #@coming_games = Game.coming
   end
 end
